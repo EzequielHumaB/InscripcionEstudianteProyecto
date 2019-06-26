f1b150784ed97b6d962e43574de1a705d3af3e1c
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-26 17:11:19 -0400

    Creacion del contexto y de RepositorioBase generico

---
+++ EstudianteInscripcionProyecto/BLL/IInterfazBLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace EstudianteInscripcionProyecto.BLL
+{
+   public interface IInterfazBLL<T> where T : class
+    {
+        List<T> GetList(Expression<Func<T, bool>> expression);
+        T Buscar(int id);
+        bool Guardar(T entity);
+
+        bool Eliminar(int id);
+        bool Modificar(T entity);
+    }
+}
---
+++ EstudianteInscripcionProyecto/BLL/RepositorioBaseBLL.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+using EstudianteInscripcionProyecto.DAL;
+using System.Data.Entity;
+
+namespace EstudianteInscripcionProyecto.BLL
+{
+    public class RepositorioBaseBLL<T> : IDisposable, IInterfazBLL<T> where T : class
+    {
+        internal Contexto _contexto;
+
+        public RepositorioBaseBLL()
+        {
+            _contexto = new Contexto();
+        }
+        
+        public virtual bool Guardar(T entity)
+        {
+            bool paso = false;
+            try
+            {
+                if (_contexto.Set<T>().Add(entity)!=null)
+                {
+                    paso = _contexto.SaveChanges() > 0;
+                }
+            }catch
+            {
+                throw;
+            }
+            finally
+            {
+                _contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public virtual bool Eliminar(int id)
+        {
+            bool paso = false;
+            try
+            {
+                T entity = _contexto.Set<T>().Find(id);
+                _contexto.Set<T>().Remove(entity);
+                paso = _contexto.SaveChanges() > 0;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                _contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public virtual bool Modificar(T entity)
+        {
+            bool paso = false;
+            try
+            {
+                _contexto.Entry(entity).State = EntityState.Modified;
+                paso = _contexto.SaveChanges() > 0;
+
+            }catch
+            {
+                throw;
+            }
+            finally
+            {
+                _contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public virtual T Buscar(int id)
+        {
+            T entity;
+            try
+            {
+                entity = _contexto.Set<T>().Find(id);
+            }catch
+            {
+                throw;
+            }
+            finally
+            {
+                _contexto.Dispose();
+            }
+            return entity;
+        }
+
+        public List<T> GetList(Expression<Func<T,bool>> expression)
+        {
+            List<T> lista = new List<T>();
+            try
+            {
+                lista = _contexto.Set<T>().Where(expression).ToList();
+            }catch
+            {
+                throw;
+            }
+            finally
+            {
+                _contexto.Dispose();
+            }
+            return lista;
+        }
+       
+
+        public void Dispose()
+        {
+            _contexto.Dispose();
+        }
+
+     
+    }
+}
