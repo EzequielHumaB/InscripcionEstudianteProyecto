ea4b94460469f53ac637cfe1afe65633b1e04f44
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-26 13:52:10 -0400

    Creacion de la carpeta Entidades, y creacion de la entidad Estudiantes

---
+++ EstudianteInscripcionProyecto/Entidades/Estudiantes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EstudianteInscripcionProyecto.Entidades
+{
+   public class Estudiantes
+    {
+        [Key]
+        public int EstudianteId { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Nombres { get; set; }
+        public decimal Balance { get; set; }
+
+        public Estudiantes()
+        {
+            EstudianteId = 0;
+            FechaIngreso = DateTime.Now;
+            Nombres = string.Empty;
+            Balance = 0;
+        }
+
+    }
+}
