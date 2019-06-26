5b15ed0c9a37e42121e0bba0bee35433347d7d65
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-26 16:11:09 -0400

    Creacion de Inscripcion y su Detalle

---
+++ EstudianteInscripcionProyecto/Entidades/DetalleInscripciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EstudianteInscripcionProyecto.Entidades
+{
+   public class DetalleInscripciones
+    {
+        [Key]
+        public int DetalleInscripcionId { get; set; }
+        public int InscripcionId { get; set; }
+        public decimal Monto { get; set; }
+        public DetalleInscripciones()
+        {
+            DetalleInscripcionId = 0;
+            InscripcionId = 0;
+            Monto = 0;
+        }
+
+        public DetalleInscripciones(int InscripcionDetalleId, int Idinscripcion, decimal totalMonto)
+        {
+            DetalleInscripcionId = InscripcionDetalleId;
+            InscripcionId = Idinscripcion;
+            Monto = totalMonto;
+        }
+    }
+}
---
+++ EstudianteInscripcionProyecto/Entidades/Inscripciones.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EstudianteInscripcionProyecto.Entidades
+{
+   public class Inscripciones
+    {
+        [Key]
+        public int InscripcionesId { get; set; }
+        public DateTime FechaInscripcion { get; set; }
+        public decimal Monto { get; set; }
+        public Inscripciones()
+        {
+            this.InscripcionesId = 0;
+            FechaInscripcion = DateTime.Now;
+            Monto = 0;
+        }
+
+    }
+}
